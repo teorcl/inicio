d493da81b30c16a02cf467d4f832e2aa04874cf5
teorcl <teo.calle.lara@gmail.com>
2021-06-17 17:53:02 -0500

    Initial Mi primera instruccion

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String frase1 = "hola";
+        String frase2 = "mundo";
+        System.out.println(frase1+" "+frase2);
+
+    }
+}
